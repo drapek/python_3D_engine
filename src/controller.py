0b0ffba7ae9eb3c9a968405a458040b13dbd07cf
Paweł Drapiewski <drapek39@wp.pl>
2017-11-06 20:07:27 +0100

    Add missing files (almost all files was missing :D)

---
+++ src/controller.py
@@ -0,0 +1,61 @@
+import sys
+
+import pygame
+
+import settings
+
+
+def keyboard_controller(scene):
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    pressed = pygame.key.get_pressed()
+
+    # moving on scene
+    if pressed[pygame.K_w]:
+        scene.move_objects_on_scene([0, 0, -settings.move_step])
+
+    if pressed[pygame.K_s]:
+        scene.move_objects_on_scene([0, 0, settings.move_step])
+
+    if pressed[pygame.K_a]:
+        scene.move_objects_on_scene([settings.move_step, 0, 0])
+
+    if pressed[pygame.K_d]:
+        scene.move_objects_on_scene([-settings.move_step, 0, 0])
+
+    # flying mode - go up and down
+    if pressed[pygame.K_p]:
+        scene.move_objects_on_scene([0, -settings.move_step, 0])
+
+    if pressed[pygame.K_l]:
+        scene.move_objects_on_scene([0, settings.move_step, 0])
+
+    # scene rotating
+    if pressed[pygame.K_UP]:
+        scene.rotate_x_objects_on_scene(-settings.camera_tilt_angle)
+
+    if pressed[pygame.K_DOWN]:
+        scene.rotate_x_objects_on_scene(settings.camera_tilt_angle)
+
+    if pressed[pygame.K_LEFT]:
+        scene.rotate_y_objects_on_scene(-settings.camera_tilt_angle)
+
+    if pressed[pygame.K_RIGHT]:
+        scene.rotate_y_objects_on_scene(settings.camera_tilt_angle)
+
+    if pressed[pygame.K_q]:
+        scene.rotate_z_objects_on_scene(-settings.camera_tilt_angle)
+
+    if pressed[pygame.K_e]:
+        scene.rotate_z_objects_on_scene(settings.camera_tilt_angle)
+
+    # Change the camera focus
+    if pressed[pygame.K_m]:
+        scene.change_camera_zoom(settings.camera_zoom_step)
+
+    if pressed[pygame.K_n]:
+        scene.change_camera_zoom(-settings.camera_zoom_step)
+
---
+++ src/scenes/objects/cuboid.py
@@ -0,0 +1,48 @@
+import pygame
+
+from camera.functions import project_3d_point_to_2d
+import settings
+
+
+class Cuboid:
+    """
+        Cuboid object.
+        Cuboid parameter to scene parameters:
+        width = X-axis
+        deep = Y-axis
+        height = Z-axis
+    """
+    nodes = None
+    edges = [(0, 1), (1, 2), (2, 3), (3, 0), (4, 5), (5, 6),
+             (6, 7), (7, 4), (0, 4), (1, 5), (2, 6), (3, 7)]
+
+    def __init__(self, width, deep, height, start_point=None):
+        """
+        4 dimensions because it is normal coordinate system
+        :param width:
+        :param deep:
+        :param height:
+        :param start_point:
+        """
+        if start_point is None:
+            start_point = [0, 0, 0, 1]
+        # if specified start point has only 3 dimensions (x, y, z), add additional 1 to get 'normal' coordinates
+        if len(start_point) == 3:
+            start_point.append(1)
+
+        self.nodes = []
+        self.nodes.append(start_point)
+        self.nodes.append([start_point[0] + width, start_point[1], start_point[2], 1])
+        self.nodes.append([start_point[0] + width, start_point[1] + deep, start_point[2], 1])
+        self.nodes.append([start_point[0], start_point[1] + deep, start_point[2], 1])
+        self.nodes.append([start_point[0], start_point[1], start_point[2] + height, 1])
+        self.nodes.append([start_point[0] + width, start_point[1], start_point[2] + height, 1])
+        self.nodes.append([start_point[0] + width, start_point[1] + deep, start_point[2] + height, 1])
+        self.nodes.append([start_point[0], start_point[1] + deep, start_point[2] + height, 1])
+
+    def draw_on_screen(self, screen, observer_distance):
+        for edge in self.edges:
+            line_from = project_3d_point_to_2d(self.nodes[edge[0]], observer_distance)
+            line_to = project_3d_point_to_2d(self.nodes[edge[1]], observer_distance)
+            pygame.draw.line(screen, settings.line_color, line_from, line_to, settings.line_thickness)
+
