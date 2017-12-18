Fix light rotation

src/camera/functions.py
@@ -55,6 +55,8 @@ def rotate_z(obj, angle):
 
 
 def rotate_one_point_x(point, angle):
+    if len(point) < 4:
+        point.append(1)
     rotate_matrix = [[1, 0, 0, 0],
                      [0, math.cos(angle), -math.sin(angle), 0],
                      [0, math.sin(angle), math.cos(angle), 0],
@@ -69,7 +71,7 @@ def rotate_one_point_y(point, angle):
                      [0, 1, 0, 0],
                      [-math.sin(angle), 0, math.cos(angle), 0],
                      [0, 0, 0, 1]]
-    return np.matmul(rotate_matrix, point)[:3]
+    return np.matmul(rotate_matrix, point)
 
 
 def multiply_object_by_matrix(obj, matrix):

src/scenes/light.py
@@ -7,9 +7,9 @@ class Light:
     max_light = None
     light_range = None
 
-    def __init__(self, light_position=None, min_light=0.1, max_light=0.9):
+    def __init__(self, light_position=None, min_light=0.1, max_light=0.7):
         if light_position is None:
-            light_position = [-1, 1, -1]
+            light_position = [-0.62, 0.12, -1.52]
 
         self.position = light_position
         self.min_light = min_light
@@ -25,7 +25,7 @@ class Light:
             # Only draw faces that face us
             if towards_us > 0:
                 normal /= np.linalg.norm(normal)
-                theta = np.dot(normal, self.position)
+                theta = np.dot(normal, self.position[:3])
 
                 if theta < 0:
                     shade = self.min_light * np.array(polygon.color)

src/scenes/scene.py
@@ -22,7 +22,6 @@ class Scene:
         project it to 2D image.
         :return:
         """
-        # print('debug')  # TODO debug only
         # sort the triangles by it's 'z' coordinate in descending order. (Painter's algorithm)
         self.available_basic_polygons.sort(key=lambda x: np.mean(x.get_z_cords()), reverse=True)
         for polygon in self.available_basic_polygons:
@@ -49,7 +48,7 @@ class Scene:
         self.observer_to_canvas_dist += step
 
     def rotate_light_x(self, angle):
-        rotate_one_point_x(self.light_source.position, angle)
+        self.light_source.position = rotate_one_point_x(self.light_source.position, angle)
 
     def rotate_light_y(self, angle):
-        rotate_one_point_y(self.light_source.position, angle)
+        self.light_source.position = rotate_one_point_y(self.light_source.position, angle)